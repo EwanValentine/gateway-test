dd16d23755ad72536ea1dd9ee2083798c7c392d6
Ewan Valentine <ewan.valentine@bbc.co.uk>
2017-10-25 00:12:43 +0100

    ADDED api

---
+++ api.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+
+	"github.com/golang/glog"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	hello "github.com/ewanvalentine/gateway-test/proto/greeter"
+)
+
+var (
+	// the go.micro.srv.greeter address
+	endpoint = flag.String("endpoint", "localhost:50051", "go.micro.srv.greeter address")
+)
+
+func run() error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+
+	err := hello.RegisterGreeterHandlerFromEndpoint(ctx, mux, "localhost:9000", opts)
+	if err != nil {
+		return err
+	}
+
+	return http.ListenAndServe(":8080", mux)
+}
+
+func main() {
+	flag.Parse()
+
+	defer glog.Flush()
+
+	if err := run(); err != nil {
+		glog.Fatal(err)
+	}
+}
